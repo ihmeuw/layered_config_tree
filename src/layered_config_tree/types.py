685d1664be502dd25b7b9b764f884505c5be54e5
Hussain Jafari <hussainsjafari@gmail.com>
2024-10-18 10:43:08 -0700

    import lct

--- src/layered_config_tree/types.py
+++ src/layered_config_tree/types.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Any, Union
+from typing import Any, TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from layered_config_tree import LayeredConfigTree
 
 # Data input types
 InputData = Union[dict[str, Any], str, Path, "LayeredConfigTree"]
